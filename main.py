e74db95bb11eee0388751b43f6a33588e9559524
Eric Hofmann <HofmannEric@dv-schulen.de>
2024-02-22 12:38:33 +0100

    animationsanfang

--- main.py
+++ main.py
@@ -14,18 +14,50 @@ height = 20
 
 vel = 10
 
+event_rect = pygame.Rect(x, y, width, height)
+
 run = True
+
+
+def fallen():
+    event_rect.x += 1
+    pygame.draw.rect(win, (255, 0, 0), event_rect)
+    pygame.time.wait(100)
+    event_rect.y += 1
+    pygame.draw.rect(win, (255, 0, 0), event_rect)
+    pygame.time.wait(100)
+    event_rect.y += 1
+    pygame.draw.rect(win, (255, 0, 0), event_rect)
+    pygame.time.wait(100)
+    event_rect.y += 1
+    pygame.draw.rect(win, (255, 0, 0), event_rect)
+    pygame.time.wait(100)
+    event_rect.y += 1
+    pygame.draw.rect(win, (255, 0, 0), event_rect)
+    pygame.time.wait(100)
+    event_rect.y += 1
+    pygame.draw.rect(win, (255, 0, 0), event_rect)
+    pygame.time.wait(100)
+    event_rect.y += 1
+    pygame.draw.rect(win, (255, 0, 0), event_rect)
+    pygame.time.wait(100)
+    event_rect.y += 1
+    pygame.draw.rect(win, (255, 0, 0), event_rect)
+
+
 
 while run:
     pygame.time.delay(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            y += vel
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and event_rect.collidepoint(event.pos):
+            event_rect.y += vel
+            fallen()
+
     win.fill((0, 0, 0))
 
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    pygame.draw.rect(win, (255, 0, 0), event_rect)
 
     pygame.display.update()
 
