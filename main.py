bce2bc58468a77bd03f24b1067cd61dbe9b3af92
Eric Hofmann <HofmannEric@dv-schulen.de>
2024-02-28 10:10:08 +0100

    sprites nötig, shit

--- main.py
+++ main.py
@@ -2,62 +2,60 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((750, 500))
 
 pygame.display.set_caption("Moving rectangle")
+scoreInt = 0
+score = "Score: " + str(scoreInt)
+ui = pygame.font.Font
 
 x = 200
 y = 200
+xx = 300
+yy = 300
 
 width = 20
 height = 20
+widthh = 25
+heightt = 25
+
+font = pygame.font.Font('freesansbold.ttf', 18)
 
 vel = 10
 
-event_rect = pygame.Rect(x, y, width, height)
+# SpriteGroups
+all_Chickens = pygame.sprite.Group()
+
+small_Chicken = pygame.Rect(x, y, width, height)
+medium_Chicken = pygame.Rect(xx, yy, widthh, heightt)
+big_Chicken = pygame.sprite.Sprite()
+# all_Chickens.add(small_Chicken)
 
 run = True
 
-
-def fallen():
-    event_rect.x += 1
-    pygame.draw.rect(win, (255, 0, 0), event_rect)
-    pygame.time.wait(100)
-    event_rect.y += 1
-    pygame.draw.rect(win, (255, 0, 0), event_rect)
-    pygame.time.wait(100)
-    event_rect.y += 1
-    pygame.draw.rect(win, (255, 0, 0), event_rect)
-    pygame.time.wait(100)
-    event_rect.y += 1
-    pygame.draw.rect(win, (255, 0, 0), event_rect)
-    pygame.time.wait(100)
-    event_rect.y += 1
-    pygame.draw.rect(win, (255, 0, 0), event_rect)
-    pygame.time.wait(100)
-    event_rect.y += 1
-    pygame.draw.rect(win, (255, 0, 0), event_rect)
-    pygame.time.wait(100)
-    event_rect.y += 1
-    pygame.draw.rect(win, (255, 0, 0), event_rect)
-    pygame.time.wait(100)
-    event_rect.y += 1
-    pygame.draw.rect(win, (255, 0, 0), event_rect)
-
-
-
 while run:
-    pygame.time.delay(10)
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and event_rect.collidepoint(event.pos):
-            event_rect.y += vel
-            fallen()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and small_Chicken.collidepoint(event.pos):
+            small_Chicken.y = 500
+            scoreInt -= 1
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and medium_Chicken.collidepoint(event.pos):
+            medium_Chicken.y = 500
+            scoreInt += 1
+    small_Chicken.x += 1
+    medium_Chicken.x += 2
+    pygame.draw.rect(win, (255, 0, 0), small_Chicken)
+    pygame.draw.rect(win, (255, 255, 0), medium_Chicken)
 
     win.fill((0, 0, 0))
 
-    pygame.draw.rect(win, (255, 0, 0), event_rect)
+    score = "Score: " + str(scoreInt)
+    text = font.render(score, True, (255, 0, 0))
+    win.blit(text, (10, 10))
+    pygame.draw.rect(win, (255, 0, 0), small_Chicken)
+    pygame.draw.rect(win, (255, 255, 0), medium_Chicken)
 
     pygame.display.update()
 
