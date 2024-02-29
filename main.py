00b76eb7a8a732bdbb5a62cc93e67e6e1889265c
Eric Hofmann <HofmannEric@dv-schulen.de>
2024-02-29 12:42:52 +0100

    botched collision

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
 import pygame
 
+
+class SmallChicken(pygame.sprite.Sprite):
+    radius = int(10)
+    trueX = bool(False)
+    trueY = bool(False)
+
+    def __init__(self, x, y, vel_x, vel_y):
+        super().__init__()
+        self.position = (x, y)
+        self.velocity = (vel_x, vel_y)
+
+    def update(self):
+        self.position = (self.position[0] + self.velocity[0], self.position[1] + self.velocity[1])
+
+    def draw(self):
+        pygame.draw.circle(win, (255, 0, 0), self.position, self.radius)
+
+    def die(self):
+        self.position = (-15, -15)
+
+    def move(self):
+        self.position = (self.position[0] + 1, self.position[1])
+
+    def posCheck(self, eventpos):
+        if self.position[0] + self.radius >= eventpos[0] >= self.position[0] - self.radius:
+            self.trueX = True
+        else:
+            self.trueX = True
+        if self.position[1] + self.radius >= eventpos[1] >= self.position[1] - self.radius:
+            self.trueY = True
+        else:
+            self.trueY = True
+
+        if self.trueY and self.trueX:
+            return True
+        else:
+            return False
+
+
 pygame.init()
 
 win = pygame.display.set_mode((750, 500))
@@ -31,6 +70,8 @@ medium_Chicken = pygame.Rect(xx, yy, widthh, heightt)
 big_Chicken = pygame.sprite.Sprite()
 # all_Chickens.add(small_Chicken)
 
+testObj = SmallChicken(x, y, vel, vel)
+
 run = True
 
 while run:
@@ -38,24 +79,42 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and small_Chicken.collidepoint(event.pos):
-            small_Chicken.y = 500
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and testObj.posCheck(event.pos) == True:
+            print(event.pos)
+            print(testObj.position)
             scoreInt -= 1
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and medium_Chicken.collidepoint(event.pos):
-            medium_Chicken.y = 500
-            scoreInt += 1
-    small_Chicken.x += 1
-    medium_Chicken.x += 2
-    pygame.draw.rect(win, (255, 0, 0), small_Chicken)
-    pygame.draw.rect(win, (255, 255, 0), medium_Chicken)
+
+
+
+
+
+
+
+
+
+
+
+
+        # if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and medium_Chicken.collidepoint(event.pos):
+        #   medium_Chicken.y = 500
+        #   scoreInt += 1
+    testObj.move()
+    # small_Chicken.x += 1
+    # medium_Chicken.x += 2
+    testObj.draw()
+    # pygame.draw.rect(win, (255, 0, 0), small_Chicken)
+    # pygame.draw.rect(win, (255, 255, 0), medium_Chicken)
 
     win.fill((0, 0, 0))
 
     score = "Score: " + str(scoreInt)
     text = font.render(score, True, (255, 0, 0))
     win.blit(text, (10, 10))
-    pygame.draw.rect(win, (255, 0, 0), small_Chicken)
-    pygame.draw.rect(win, (255, 255, 0), medium_Chicken)
+
+    testObj.draw()
+
+    # pygame.draw.rect(win, (255, 0, 0), small_Chicken)
+    # pygame.draw.rect(win, (255, 255, 0), medium_Chicken)
 
     pygame.display.update()
 
